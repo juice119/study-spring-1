99e26d8e7e0bca99c9904a1e41624841b33d47e8
Juice <3221ju@gmail.com>
2022-03-31 02:40:18 +0900

    up: [STU-3] 미사용 파일 제거

--- hello-spring/src/main/java/com/hello/hellospring/controller/UserController.java
+++
@@ -1,8 +0,0 @@
-package com.hello.hellospring.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class UserController {
-}
