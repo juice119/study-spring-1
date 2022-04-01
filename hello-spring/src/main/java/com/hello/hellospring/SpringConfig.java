7bb6f645072ac9614027bde39cf6004a558b83f0
Juice <3221ju@gmail.com>
2022-04-02 00:32:03 +0900

    new: [STU-3] Spring JPA 추가

--- hello-spring/src/main/java/com/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/com/hello/hellospring/SpringConfig.java
@@ -1,21 +1,28 @@
 package com.hello.hellospring;
 
-import com.hello.hellospring.repository.JpaMemberRepository;
 import com.hello.hellospring.repository.MemberRepository;
 import com.hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.persistence.EntityManager;
 
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
+    private final MemberRepository memberRepository;
 
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
+
+
+
+//    private EntityManager em;
+//    @Autowired
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
 
 //    private final DataSource dataSource;
 //    public SpringConfig(DataSource dataSource) {
@@ -24,14 +31,15 @@ public class SpringConfig {
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-// return new MemoryMemberRepository();
-//        return new JdbcMemberRepository(dataSource);
-//        return new JdbcTemplateRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+//// return new MemoryMemberRepository();
+////        return new JdbcMemberRepository(dataSource);
+////        return new JdbcTemplateRepository(dataSource);
+//
+//        return new JpaMemberRepository(em);
+//    }
 }
