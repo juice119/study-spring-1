e23ac635bd07e4eea3663b4c7d6c458c6d79bfb9
Juice <3221ju@gmail.com>
2022-04-01 22:06:57 +0900

    new: [STU-3] JDBC e2e 테스트 추가

--- hello-spring/src/test/java/com/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ hello-spring/src/test/java/com/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -1,44 +1,36 @@
 package com.hello.hellospring.service;
 
 import com.hello.hellospring.domain.Member;
-import com.hello.hellospring.repository.MemoryMemberRepository;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import com.hello.hellospring.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
-class MemberServiceTest {
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
 
-    MemberService memberService;
-    MemoryMemberRepository memberRepository;
-
-    @BeforeEach
-    public void beforeEach() {
-        memberRepository = new MemoryMemberRepository();
-        memberService = new MemberService(memberRepository);
-    }
-
-    @AfterEach
-    public void afterEach() {
-        memberRepository.clearStore();
-    }
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
 
     @Test
-    void 회원가입() {
-        // given
+    void 회원가입() throws Exception {
+        //given
         Member member1 = new Member();
-        member1.setName("hello");
+        member1.setName("spring");
 
         // when
         Long saveId = memberService.join(member1);
 
         // then
         Member findMember = memberService.findOne(saveId).get();
-        assertThat(member1).isEqualTo(findMember);
+        assertThat(member1.getName()).isEqualTo(findMember.getName());
     }
 
     @Test
@@ -58,8 +50,9 @@ class MemberServiceTest {
         assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원 입니다.");
     }
 
+
     @Test
-    void findMembers() {
+    void 사용자리스트() {
         // given
         Member member1 = new Member();
         member1.setName("hello1");
@@ -71,22 +64,13 @@ class MemberServiceTest {
         memberService.join(member1);
         memberService.join(member2);
         List<Member> members = memberService.findMembers();
-
-        assertThat(members.size()).isEqualTo(2);
     }
 
     @Test
     void findOne() {
-        // given
-        String memberName = "hello1";
-        Member member1 = new Member();
-        member1.setName(memberName);
-
-        // when
-        Long id = memberService.join(member1);
-        Member member = memberService.findOne(id).get();
-
-        // then
-        assertThat(member.getName()).isEqualTo(memberName);
     }
+
+    //given
+    //when
+    //then
 }
